6e992aea52092495dc2feffc4afeef426c9230d8
Ousmane Seidy Diallo <omudu45@gmail.com>
2018-04-13 01:13:46 +0000

    feat: add accordion.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-console.log('it works');
-console.log('Yeh Yeh Yeh');
+/*
+ * Accordion handler
+ */
+const acc = document.getElementsByClassName('accordion');
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle('active');
+
+    /* Toggle between hidding and showing the active panel */
+    const panel = this.nextElementSibling;
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
+    } else {
+      panel.style.display = 'block';
+    }
+  });
+}
