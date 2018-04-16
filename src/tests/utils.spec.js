d71a938322e6f1b7ce391e93001cdfb7d8d431b1
Ousmane Seidy Diallo <mudu.jalloh@gn.ehealthafrica.org>
2018-04-16 21:27:21 +0000

    fix: make tests more discriptive.

--- src/tests/utils.spec.js
+++ src/tests/utils.spec.js
@@ -6,21 +6,21 @@ const utils = require('../utils');
 describe('Utils', () => {
   /* test isEqual utility function */
   describe('isEqual (Arrys)', () => {
-    it('should not be equal', () => {
+    it('should not be equal if content are different', () => {
       const firstArr = ['one','tow','three'];
       const secondArr = ['one','2', 'three'];
 
       should(utils.isEqual(firstArr, secondArr)).be.false();
     })
 
-    it('should be equal', () => {
+    it('should be equal if content are the same', () => {
       const item1 = ['one', 2, 'three', 4, 'five'];
       const item2 = [...item1];
 
       should(utils.isEqual(item1, item2)).be.true();
     })
 
-    it('should be equal', () => {
+    it('should be equal if array object are the same', () => {
       const item1 = [
         {'name': 'Sam', 'age': 20, 'number': 50},
         {'name': 'James', 'age': 30, 'number': 20}
@@ -32,18 +32,18 @@ describe('Utils', () => {
   })
 
   describe('isEqual (Objects)', () => {
-    it('should be equal', () => {
-      const obj1 = {'one': [1,2,3], 'two': 2, 'three': {'age': 20}};
-      const obj2 = {...obj1};
-
-      should(utils.isEqual(obj1, obj2)).be.true();
-    })
-
-    it('should not be equal', () => {
+    it('should not be equal if objects are different', () => {
       const ob1 = {'age': 20, 'name': 'Doe'};
       const ob2 = {'age': 30, 'name': 'Doe'};
 
       should(utils.isEqual(ob1, ob2)).be.false();
+    })
+    
+    it('should be equal if objects are the same', () => {
+      const obj1 = {'one': [1,2,3], 'two': 2, 'three': {'age': 20}};
+      const obj2 = {...obj1};
+
+      should(utils.isEqual(obj1, obj2)).be.true();
     })
   })
 })
